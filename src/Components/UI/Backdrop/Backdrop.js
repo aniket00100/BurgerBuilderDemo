1d4a2e64483881e0ef6c04f344943c181b7d1c79
aniket00100 <aniketkale.ak@gmail.com>
2019-07-15 21:31:00 +0530

    one final change

--- src/Components/UI/Backdrop/Backdrop.js
+++ src/Components/UI/Backdrop/Backdrop.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './Backdrop.module.css';
 
 const backdrop = (props) => (
-    props.show ? <div className={classes.Backdrop} onClick={props.closed}></div> : null
+    props.show ? <div className={classes.Backdrop} onClick={props.cancel}></div> : null
 )
 
 export default backdrop;
--- src/Components/UI/Modal/Modal.js
+++ src/Components/UI/Modal/Modal.js
@@ -6,7 +6,7 @@ import Wrapper from '../../hoc/wrapper';
 const modal = (props) => {
     return (
         <Wrapper>
-            <Backdrop show={props.show} cancel={props.modalCancel} closed={props.cancel}/>
+            <Backdrop show={props.show} cancel={props.modalCancel}/>
             <div style={{
                     transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
                     opacity: props.show ? '1' : '0'}}
